3b2c7842ac427d0f1eab35cae01b51f37c2bdc2a
UglyGeorge <yuriy.n.russkikh@gmail.com>
2024-10-04 17:43:56 +0300

    Added more custom action types.

---
+++ MediaOrganizer/MediaOrganizer/Models/ElementOptions.swift
@@ -0,0 +1,8 @@
+//
+//  ElementOptions.swift
+//  MediaOrganizer
+//
+//  Created by Yuriy Russkikh on 02.10.2024.
+//
+
+import Foundation
---
+++ MediaOrganizer/MediaOrganizer/Views/ActionElementEditView.swift
@@ -0,0 +1,8 @@
+//
+//  ActionElementEditView.swift
+//  MediaOrganizer
+//
+//  Created by Yuriy Russkikh on 02.10.2024.
+//
+
+import Foundation
